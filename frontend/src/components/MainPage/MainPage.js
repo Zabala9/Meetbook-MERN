ee9b5734d9fbffeb81c4749aebff9a21b49a906f
Steven Zabala <zabalasteven9@gmail.com>
2023-08-08 16:40:26 -0700

    changes to description in main page

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -7,14 +7,16 @@ function MainPage(){
             <div>
                 <div id='div-left'>
                     <label id='label-title'>Meetbook</label>
-                    <p>
-                        Unlock a world of connections with Meetbook!
-                    </p>
                     <label id='label1'>
-                        Your go-to platform for sharing, discovering, 
+                        Unlock a world of connections with Meetbook!
+                    </label>
+                    <label id='label2'>
+                        Your go-to platform for sharing, discovering,
+                    </label>
+                    <label id='label3'>
                         and staying close with friends.
                     </label>
-                    <label id='label2'>Start sharing your story today!</label>
+                    <label id='label4'>Start sharing your story today!</label>
                 </div>
                 <div id='div-right'>
                     <LoginForm />
