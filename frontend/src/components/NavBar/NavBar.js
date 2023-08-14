e0f9911de11e7f7ed4fef31545f3a4e3d1c393c1
Steven Zabala <zabalasteven9@gmail.com>
2023-08-14 14:12:46 -0700

    Styling navbar and adding dropmenu with photoProfile

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
@@ -7,7 +8,25 @@ import './NavBar.css';
 function NavBar () {
     const history = useHistory();
     const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
     const user = useSelector(state => state.session.user);
+
+    const openMenu = () => {
+        if(showMenu) return;
+        setShowMenu(true);
+    };
+
+    useEffect(() => {
+        if(!showMenu) return;
+
+        const closeMenu = () => {
+            setShowMenu(false);
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu]);
 
     const logoutUser = e => {
         e.preventDefault();
@@ -18,23 +37,33 @@ function NavBar () {
 
     return (
         <div className='container-navbar'>
-            <div id='container-right-side-nav'>
+            <div id='left-side-nav'>
                 <Link id='link-main' to='/'>
                     <img src={image} alt='' id='img-main'/>
                 </Link>
-                <Link id='link-profile' to='/profile'>
-                    Profile
-                </Link>
             </div>
-            <div id='container-left-side-nav'>
-                <button id='user-button'>
-                    <img src={user.profileImageUrl} alt='' id='profile-img'/>
-                </button>
-                <button id='logout-button'
-                    onClick={logoutUser}
-                >
-                    Log out
-                </button>
+            <div id='right-side-nav'>
+                <div id='dropdown-navbar' style={{ textAlign: 'right' }}>
+                    <button id='button-dropdown' onClick={openMenu}>
+                        <img src={user.profileImageUrl} alt='' id='profile-img'/>
+                        <i className="fa-solid fa-caret-down" id='arrow-drop'></i>
+                    </button>
+                </div>
+                { showMenu && (
+                    <div className='dropdown-content'>
+                        <Link id='link-profile' to='/profile'>
+                            Profile
+                        </Link>
+                        <Link to='/about' id='about-link'> About us
+                            <i className="fa-regular fa-circle-question" id='about-link-img'></i>
+                        </Link>
+                        <button id='logout-button'
+                            onClick={logoutUser}
+                        > Log out
+                            <i className="fa-solid fa-arrow-right-from-bracket" id='logout-img'></i>
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     )
