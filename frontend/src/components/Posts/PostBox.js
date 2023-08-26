9df8724b12f5beedaedc9816317f2139e437a720
Steven Zabala <zabalasteven9@gmail.com>
2023-08-26 06:14:44 -0700

    Making appear private post only on profile

--- frontend/src/components/Posts/PostBox.js
+++ frontend/src/components/Posts/PostBox.js
@@ -4,35 +4,66 @@ import './PostBox.css';
 
 function PostBox ({ post: {text, author, imageUrls, privacy }}) {
     const { name, lastname, profileImageUrl, _id } = author;
+    const location = window.location.pathname;
+    console.log(location);
 
     const images = imageUrls?.map((url, index) => {
         return <img id='post-image' key={url} src={url} alt='' />
     });
 
     return (
-        <div className='container-post'>
-            <div className='container-post-info'>
-                <label id='label-name-postbox'>
-                    <Link to='/'>
-                        <img id='profile-image' src={profileImageUrl} alt='' />
-                    </Link>
-                    <div className='container-name-privacy-postbox'>
-                        <Link id='name-user-postbox' to='/'>{name + ' ' + lastname}</Link>
-                        <label id='privacy-postbox'>
-                            {privacy === 'public' ? <i className="fa-solid fa-earth-americas" id='img-public-privacy-postbox'></i> :
-                                privacy === 'friends' ? <i className="fa-solid fa-user-group" id='img-friends-privacy-postbox'></i> : 
-                                privacy === 'only me' ? <i className="fa-solid fa-lock" id='img-onlyme-privacy-postbox'></i> : undefined
-                            }
+        <>
+            { location === '/feed' && privacy === 'only me' ? undefined :
+                location === '/feed' && (privacy === 'friends' || privacy === 'public') ? 
+                <div className='container-post'>
+                    <div className='container-post-info'>
+                        <label id='label-name-postbox'>
+                            <Link to='/'>
+                                <img id='profile-image' src={profileImageUrl} alt='' />
+                            </Link>
+                            <div className='container-name-privacy-postbox'>
+                                <Link id='name-user-postbox' to='/'>{name + ' ' + lastname}</Link>
+                                <label id='privacy-postbox'>
+                                    {privacy === 'public' ? <i className="fa-solid fa-earth-americas" id='img-public-privacy-postbox'></i> :
+                                        privacy === 'friends' ? <i className="fa-solid fa-user-group" id='img-friends-privacy-postbox'></i> : 
+                                        privacy === 'only me' ? <i className="fa-solid fa-lock" id='img-onlyme-privacy-postbox'></i> : undefined
+                                    }
+                                </label>
+                            </div>
                         </label>
+                        <PostButton id={_id} />
                     </div>
-                </label>
-                <PostButton id={_id} />
-            </div>
-            <div id='container-text-post-postbox'>
-                <p id='label-text-post' >{text}</p>
-            </div>
-            {images}
-        </div>
+                    <div id='container-text-post-postbox'>
+                        <p id='label-text-post' >{text}</p>
+                    </div>
+                    {images}
+                </div> : 
+                location === '/profile' ? 
+                    <div className='container-post'>
+                        <div className='container-post-info'>
+                            <label id='label-name-postbox'>
+                                <Link to='/'>
+                                    <img id='profile-image' src={profileImageUrl} alt='' />
+                                </Link>
+                                <div className='container-name-privacy-postbox'>
+                                    <Link id='name-user-postbox' to='/'>{name + ' ' + lastname}</Link>
+                                    <label id='privacy-postbox'>
+                                        {privacy === 'public' ? <i className="fa-solid fa-earth-americas" id='img-public-privacy-postbox'></i> :
+                                            privacy === 'friends' ? <i className="fa-solid fa-user-group" id='img-friends-privacy-postbox'></i> : 
+                                            privacy === 'only me' ? <i className="fa-solid fa-lock" id='img-onlyme-privacy-postbox'></i> : undefined
+                                        }
+                                    </label>
+                                </div>
+                            </label>
+                            <PostButton id={_id} />
+                        </div>
+                        <div id='container-text-post-postbox'>
+                            <p id='label-text-post' >{text}</p>
+                        </div>
+                        {images}
+                    </div> : undefined
+            }
+        </>
     );
 }
 
