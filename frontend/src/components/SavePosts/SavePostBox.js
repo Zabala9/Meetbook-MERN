c87bd4cc9589d7964ba74c45dc6566d249bd2795
Steven Zabala <zabalasteven9@gmail.com>
2023-10-30 20:40:02 -0700

    Adding functionality to go to post show from saved post

--- frontend/src/components/SavePosts/SavePostBox.js
+++ frontend/src/components/SavePosts/SavePostBox.js
@@ -1,38 +1,39 @@
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { deleteSavePost } from '../../store/savePosts';
 import './SavePostBox.css';
 
 function SavePostBox({ post }){
     const dispatch = useDispatch();
+    const history = useHistory();
     const postInformation = post.postInformation;
     const authorInformation = post.author;
     const authorPostInformation = post.postInformation.author;
-    // let imageOwnerPost;
-    
-    // if(postInformation){
-    //     imageOwnerPost = 
-    // }
 
     const deleteSavedPost = e => {
         e.preventDefault();
         dispatch(deleteSavePost(post._id));
     };
 
-    // if(!post) return undefined;
+    const goToPostShow = e => {
+        e.preventDefault();
+        let path = `/post/${postInformation._id}`;
+        history.push(path);
+    };
 
     return(
         <div className='container-save-post-box'>
             <div className='left-side-save-post-box'>
                 {postInformation.imageUrls ?
                     postInformation.imageUrls.length > 0 ?
-                    <img src={postInformation.imageUrls} alt='' id='img-saved-post' /> :
-                    <img src={authorPostInformation.profileImageUrl} alt='' id='img-saved-post' />
+                    <img src={postInformation.imageUrls} alt='' id='img-saved-post' onClick={goToPostShow} /> :
+                    <img src={authorPostInformation.profileImageUrl} alt='' id='img-saved-post' onClick={goToPostShow} />
                     : undefined
                 }
             </div>
             <div className='rigth-side-save-post-box'>
                 <div className='container-text-save-post-box'>
-                    <p id='post-text-save-post-box'>{postInformation.text}</p>
+                    <p id='post-text-save-post-box' onClick={goToPostShow}>{postInformation.text}</p>
                     <label id='label-post-save-post-box'>Post</label>
                     <label id='label-saved-from'>Saved from
                         {authorPostInformation ? 
