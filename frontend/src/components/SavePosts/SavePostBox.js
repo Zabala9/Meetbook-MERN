295313a6806e5203f0ecd3962f66c37dbcd4368a
Steven Zabala <zabalasteven9@gmail.com>
2023-10-30 20:34:36 -0700

    Fixing problem when reloading the page save posts

--- frontend/src/components/SavePosts/SavePostBox.js
+++ frontend/src/components/SavePosts/SavePostBox.js
@@ -7,18 +7,27 @@ function SavePostBox({ post }){
     const postInformation = post.postInformation;
     const authorInformation = post.author;
     const authorPostInformation = post.postInformation.author;
+    // let imageOwnerPost;
+    
+    // if(postInformation){
+    //     imageOwnerPost = 
+    // }
 
     const deleteSavedPost = e => {
         e.preventDefault();
         dispatch(deleteSavePost(post._id));
     };
 
+    // if(!post) return undefined;
+
     return(
         <div className='container-save-post-box'>
             <div className='left-side-save-post-box'>
-                {postInformation.imageUrls.length > 0 ?
+                {postInformation.imageUrls ?
+                    postInformation.imageUrls.length > 0 ?
                     <img src={postInformation.imageUrls} alt='' id='img-saved-post' /> :
                     <img src={authorPostInformation.profileImageUrl} alt='' id='img-saved-post' />
+                    : undefined
                 }
             </div>
             <div className='rigth-side-save-post-box'>
@@ -26,7 +35,10 @@ function SavePostBox({ post }){
                     <p id='post-text-save-post-box'>{postInformation.text}</p>
                     <label id='label-post-save-post-box'>Post</label>
                     <label id='label-saved-from'>Saved from
-                        <label id='label-save-from-inside'>{authorPostInformation.name + " " + authorPostInformation.lastname}'s post</label>
+                        {authorPostInformation ? 
+                            <label id='label-save-from-inside'>{authorPostInformation.name + " " + authorPostInformation.lastname}'s post</label>
+                            : undefined
+                        }
                     </label>
                 </div>
                 <button id='share-button-save-post-box'>share here</button>
